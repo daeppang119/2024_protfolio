463af62897d421543c8a661597f3d95ed0402629
대빵 <yuna_park@DAEPPANG-MacBook-Pro.local>
2024-03-24 14:09:55 +0900

    about me 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,10 +6,25 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <div>
-      <h2 className="h-0 w-0 overflow-hidden indent-[-9999px] leading-[0px]">
-        about
-      </h2>
-    </div>
+    <>
+      <section className="h-screen p-[32px]">
+        <h2 className="h-0 w-0 overflow-hidden indent-[-9999px] leading-[0px]">
+          about
+        </h2>
+        <div className="absolute left-[5vw] top-1/2 translate-y-[-50%] text-[7vw] uppercase leading-snug text-[#6f674a] dark:text-white">
+          <p>a story of</p>
+          <p>creative fornt-end</p>
+          <p>developer</p>
+          <div className="mt-7 text-[3vw]">
+            <span className="inline-block h-px w-[400px] dark:bg-white"></span>
+            <p className="inline-block pl-2 font-thin lowercase dark:text-white">
+              about me
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="h-screen p-[32px]"></section>
+      <section className="h-screen p-[32px]"></section>
+    </>
   );
 }
